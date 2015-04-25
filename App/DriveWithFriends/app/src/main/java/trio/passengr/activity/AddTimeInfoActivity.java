b64a6d71aa3ca83cfe879bc082bf3c1c886e6407
Davis Shurbert <dshurbert@gmail.com>
2015-04-25 16:25:07 -0700

    posting route data to server

--- App/DriveWithFriends/app/src/main/java/trio/passengr/activity/AddTimeInfoActivity.java
+++ App/DriveWithFriends/app/src/main/java/trio/passengr/activity/AddTimeInfoActivity.java
@@ -11,6 +11,12 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.CheckedTextView;
 
+import com.parse.ParseObject;
+import com.parse.ParseGeoPoint;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import trio.passengr.R;
@@ -35,6 +41,8 @@ public class AddTimeInfoActivity extends Activity {
     private Calendar calendar;
     private String format = "";
 
+    private CheckedTextView[] checkedDays = new CheckedTextView[7];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +56,15 @@ public class AddTimeInfoActivity extends Activity {
         endLng = intent.getStringExtra(MapActivity.END_LNG);
 
         timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        checkedDays[0] = (CheckedTextView) findViewById(R.id.checkedSunday0);
+        checkedDays[1] = (CheckedTextView) findViewById(R.id.checkedMonday1);
+        checkedDays[2] = (CheckedTextView) findViewById(R.id.checkedTuesday2);
+        checkedDays[3] = (CheckedTextView) findViewById(R.id.checkedWednesday3);
+        checkedDays[4] = (CheckedTextView) findViewById(R.id.checkedThursday4);
+        checkedDays[5] = (CheckedTextView) findViewById(R.id.checkedFriday5);
+        checkedDays[6] = (CheckedTextView) findViewById(R.id.checkedSaturday6);
+
         calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
@@ -75,6 +92,44 @@ public class AddTimeInfoActivity extends Activity {
         intent.putExtra(END_LNG, endLng);
         intent.putExtra(HOUR, hour);
         intent.putExtra(MINUTE, min);
+
+        // create a ParseObject to be posted to the server. these objects are
+        // just rows in a table.
+        ParseObject route = new ParseObject("Route");
+
+        ParseGeoPoint startGeoPoint = new ParseGeoPoint(Double.parseDouble(startLat), Double.parseDouble(startLng));
+        ParseGeoPoint endGeoPoint = new ParseGeoPoint(Double.parseDouble(endLat), Double.parseDouble(endLng));
+        route.put("endPoint", endGeoPoint);
+        route.put("startPoint", startGeoPoint);
+
+        // read time input from user and add to our ParseObject
+        JSONArray time = new JSONArray();
+        time.put(hour);
+        time.put(min);
+        route.put("timeOfRoute", time);
+        route.put("userId", "1");
+
+        // read days of the week input from user, add the array to our route ParseObject
+        String[] textDays = new String[7];
+        textDays[0] = "sunday";
+        textDays[1] = "monday";
+        textDays[2] = "tuesday";
+        textDays[3] = "wednesday";
+        textDays[4] = "thursday";
+        textDays[5] = "friday";
+        textDays[6] = "saturday";
+        JSONArray myDays = new JSONArray();
+        for (int i=0; i<7; i++){
+            if (checkedDays[i].isChecked()) {
+                myDays.put(textDays[i]);
+            }
+        }
+        route.put("daysOfWeek",myDays);
+
+        // post our route object to the server
+        route.saveInBackground();
+
+
 
         startActivity(intent);
     }
